2755fdae88d67167c8e43e928685c80654156b0c
LiceoSalveminiBari <michele.didonna@liceosalvemini.it>
2024-06-01 12:24:39 +0200

    update

---
+++ risorse/button.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.button');
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('touchstart', function() {
+        this.click();
+      });
+    });
+  });
+  
